01004a47e1672f2181918854d5970e3d4a0a8a58
Juhi Dubey <juhidubey9906@gmail.com>
2026-06-28 17:07:13 +0530

    Added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://jobapplication-internship-management.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
